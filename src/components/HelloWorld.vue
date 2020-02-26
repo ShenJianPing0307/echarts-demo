e27eac29a2231ffccd6ebd2f3108fe461e02a49d
bright <bright@live.com>
2020-02-26 21:36:37 +0800

    加入table分页

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,64 @@
 <template>
   <div class="hello">
-    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+    <button @click="hh">切换</button>
+    <div  v-show="flag" id="myChart" :style="{width: '500px', height: '400px'}">图表</div>
+    <div  v-show="!flag">
+    <el-table
+            :data="tableDATA"
+            style="width: 100%">
+      <el-table-column
+              prop="date"
+              label="日期"
+              width="180">
+      </el-table-column>
+      <el-table-column
+              prop="name"
+              label="姓名"
+              width="180">
+      </el-table-column>
+      <el-table-column
+              prop="address"
+              label="地址">
+      </el-table-column>
+    </el-table>
+      <el-pagination
+              @prev-click="prepage"
+              @next-click="nextpage"
+              :page-size="3"
+              background
+              layout="prev, pager, next"
+              :total="gettotal">
+      </el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
   import 'echarts-liquidfill/src/liquidFill.js';
 export default {
+  data(){
+    return {
+      flag:true,
+      tableDATA:[],
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+    }
+  },
   name: 'HelloWorld',
   props: {
     msg: String
@@ -14,8 +66,33 @@ export default {
   mounted() {
     this.draw()
   },
-
+  computed:{
+    gettotal(){
+      return this.tableData.length
+    }
+  },
   methods:{
+
+
+
+    prepage(val){
+      console.log('prepage',val)
+      this.tableDATA = this.tableData.slice((val-1)*2,2*val)
+    },
+    nextpage(val){
+      console.log('nextpage',val)
+      this.tableDATA = this.tableData.slice((val-1)*2,2*val)
+    },
+
+    hh(){
+      if(this.flag === true){
+        console.log(this.flag)
+        this.flag=false
+        console.log(this.flag)
+      }else {
+        this.flag=true
+      }
+    },
     draw(){
       let myChart = this.$echarts.init(document.getElementById('myChart'))
       // myChart.setOption({
@@ -182,178 +259,178 @@ export default {
       // };
 
       //渐变柱状图
-      var option = {
-        backgroundColor:'#323a5e',
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-          }
-        },
-        grid: {
-          left: '2%',
-          right: '4%',
-          bottom: '14%',
-          top:'16%',
-          containLabel: true
-        },
-        legend: {
-          data: ['1', '2', '3'],
-          right: 10,
-          top:12,
-          textStyle: {
-            color: "#fff"
-          },
-          itemWidth: 12,
-          itemHeight: 10,
-          // itemGap: 35
-        },
-        xAxis: {
-          type: 'category',
-          data: ['2012','2013','2014','2015','2016','2017','2018','2019'],
-          axisLine: {
-            lineStyle: {
-              color: 'white'
-
-            }
-          },
-          axisLabel: {
-            // interval: 0,
-            // rotate: 40,
-            textStyle: {
-              fontFamily: 'Microsoft YaHei'
-            }
-          },
-        },
-
-        yAxis: {
-          type: 'value',
-          max:'1200',
-          axisLine: {
-            show: false,
-            lineStyle: {
-              color: 'white'
-            }
-          },
-          splitLine: {
-            show: true,
-            lineStyle: {
-              color: 'rgba(255,255,255,0.3)'
-            }
-          },
-          axisLabel: {}
-        },
-        "dataZoom": [{
-          "show": true,
-          "height": 12,
-          "xAxisIndex": [
-            0
-          ],
-          bottom:'8%',
-          "start": 10,
-          "end": 90,
-          handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
-          handleSize: '110%',
-          handleStyle:{
-            color:"#d3dee5",
-
-          },
-          textStyle:{
-            color:"#fff"},
-          borderColor:"#90979c"
-        }, {
-          "type": "inside",
-          "show": true,
-          "height": 15,
-          "start": 1,
-          "end": 35
-        }],
-        series: [{
-          name: '1',
-          type: 'bar',
-          barWidth: '15%',
-          itemStyle: {
-            normal: {
-              color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                offset: 0,
-                color: '#fccb05'
-              }, {
-                offset: 1,
-                color: '#f5804d'
-              }]),
-              barBorderRadius: 12,
-            },
-          },
-          data: [400, 400, 300, 300, 300, 400, 400, 400, 300]
-        },
-          {
-            name: '2',
-            type: 'bar',
-            barWidth: '15%',
-            itemStyle: {
-              normal: {
-                color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                  offset: 0,
-                  color: '#8bd46e'
-                }, {
-                  offset: 1,
-                  color: '#09bcb7'
-                }]),
-                barBorderRadius: 11,
-              }
-
-            },
-            data: [400, 500, 500, 500, 500, 400,400, 500, 500]
-          },
-          {
-            name: '3',
-            type: 'bar',
-            barWidth: '15%',
-            itemStyle: {
-              normal: {
-                color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                  offset: 0,
-                  color: '#248ff7'
-                }, {
-                  offset: 1,
-                  color: '#6851f1'
-                }]),
-                barBorderRadius: 11,
-              }
-            },
-            data: [400, 600, 700, 700, 1000, 400, 400, 600, 700]
-          }]
-      };
-
-      var app = {
-        currentIndex: -1,
-      };
-      setInterval(function () {
-        var dataLen = option.series[0].data.length;
-
-        // 取消之前高亮的图形
-        myChart.dispatchAction({
-          type: 'downplay',
-          seriesIndex: 0,
-          dataIndex: app.currentIndex
-        });
-        app.currentIndex = (app.currentIndex + 1) % dataLen;
-        //console.log(app.currentIndex);
-        // 高亮当前图形
-        myChart.dispatchAction({
-          type: 'highlight',
-          seriesIndex: 0,
-          dataIndex: app.currentIndex,
-        });
-        // 显示 tooltip
-        myChart.dispatchAction({
-          type: 'showTip',
-          seriesIndex: 0,
-          dataIndex: app.currentIndex
-        });
-
-
-      }, 1000);
+      // var option = {
+      //   backgroundColor:'#323a5e',
+      //   tooltip: {
+      //     trigger: 'axis',
+      //     axisPointer: { // 坐标轴指示器，坐标轴触发有效
+      //       type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+      //     }
+      //   },
+      //   grid: {
+      //     left: '2%',
+      //     right: '4%',
+      //     bottom: '14%',
+      //     top:'16%',
+      //     containLabel: true
+      //   },
+      //   legend: {
+      //     data: ['1', '2', '3'],
+      //     right: 10,
+      //     top:12,
+      //     textStyle: {
+      //       color: "#fff"
+      //     },
+      //     itemWidth: 12,
+      //     itemHeight: 10,
+      //     // itemGap: 35
+      //   },
+      //   xAxis: {
+      //     type: 'category',
+      //     data: ['2012','2013','2014','2015','2016','2017','2018','2019'],
+      //     axisLine: {
+      //       lineStyle: {
+      //         color: 'white'
+      //
+      //       }
+      //     },
+      //     axisLabel: {
+      //       // interval: 0,
+      //       // rotate: 40,
+      //       textStyle: {
+      //         fontFamily: 'Microsoft YaHei'
+      //       }
+      //     },
+      //   },
+      //
+      //   yAxis: {
+      //     type: 'value',
+      //     max:'1200',
+      //     axisLine: {
+      //       show: false,
+      //       lineStyle: {
+      //         color: 'white'
+      //       }
+      //     },
+      //     splitLine: {
+      //       show: true,
+      //       lineStyle: {
+      //         color: 'rgba(255,255,255,0.3)'
+      //       }
+      //     },
+      //     axisLabel: {}
+      //   },
+      //   "dataZoom": [{
+      //     "show": true,
+      //     "height": 12,
+      //     "xAxisIndex": [
+      //       0
+      //     ],
+      //     bottom:'8%',
+      //     "start": 10,
+      //     "end": 90,
+      //     handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+      //     handleSize: '110%',
+      //     handleStyle:{
+      //       color:"#d3dee5",
+      //
+      //     },
+      //     textStyle:{
+      //       color:"#fff"},
+      //     borderColor:"#90979c"
+      //   }, {
+      //     "type": "inside",
+      //     "show": true,
+      //     "height": 15,
+      //     "start": 1,
+      //     "end": 35
+      //   }],
+      //   series: [{
+      //     name: '1',
+      //     type: 'bar',
+      //     barWidth: '15%',
+      //     itemStyle: {
+      //       normal: {
+      //         color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+      //           offset: 0,
+      //           color: '#fccb05'
+      //         }, {
+      //           offset: 1,
+      //           color: '#f5804d'
+      //         }]),
+      //         barBorderRadius: 12,
+      //       },
+      //     },
+      //     data: [400, 400, 300, 300, 300, 400, 400, 400, 300]
+      //   },
+      //     {
+      //       name: '2',
+      //       type: 'bar',
+      //       barWidth: '15%',
+      //       itemStyle: {
+      //         normal: {
+      //           color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+      //             offset: 0,
+      //             color: '#8bd46e'
+      //           }, {
+      //             offset: 1,
+      //             color: '#09bcb7'
+      //           }]),
+      //           barBorderRadius: 11,
+      //         }
+      //
+      //       },
+      //       data: [400, 500, 500, 500, 500, 400,400, 500, 500]
+      //     },
+      //     {
+      //       name: '3',
+      //       type: 'bar',
+      //       barWidth: '15%',
+      //       itemStyle: {
+      //         normal: {
+      //           color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+      //             offset: 0,
+      //             color: '#248ff7'
+      //           }, {
+      //             offset: 1,
+      //             color: '#6851f1'
+      //           }]),
+      //           barBorderRadius: 11,
+      //         }
+      //       },
+      //       data: [400, 600, 700, 700, 1000, 400, 400, 600, 700]
+      //     }]
+      // };
+      //
+      // var app = {
+      //   currentIndex: -1,
+      // };
+      // setInterval(function () {
+      //   var dataLen = option.series[0].data.length;
+      //
+      //   // 取消之前高亮的图形
+      //   myChart.dispatchAction({
+      //     type: 'downplay',
+      //     seriesIndex: 0,
+      //     dataIndex: app.currentIndex
+      //   });
+      //   app.currentIndex = (app.currentIndex + 1) % dataLen;
+      //   //console.log(app.currentIndex);
+      //   // 高亮当前图形
+      //   myChart.dispatchAction({
+      //     type: 'highlight',
+      //     seriesIndex: 0,
+      //     dataIndex: app.currentIndex,
+      //   });
+      //   // 显示 tooltip
+      //   myChart.dispatchAction({
+      //     type: 'showTip',
+      //     seriesIndex: 0,
+      //     dataIndex: app.currentIndex
+      //   });
+      //
+      //
+      // }, 1000);
 
 
       // var option = {
@@ -500,6 +577,12 @@ export default {
       //     }
       //   }]
       // }
+      var option = {
+        series: [{
+          type: 'liquidFill',
+          data: [0.6]
+        }]
+      };
       myChart.setOption(option)
 
 
